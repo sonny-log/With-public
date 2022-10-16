17a3319c5acd2d87240d48442e8aa4d1767a31ed
soni <huyb20@naver.com>
2022-10-16 20:28:31 +0900

    share

---
+++ db/parkinglocList.js
@@ -0,0 +1,13 @@
+/* 자동차주차장 */
+var pos_car = new Array();
+
+pos_car = [
+    { latlng: new kakao.maps.LatLng(36.543535, 128.795154) },
+    { latlng: new kakao.maps.LatLng(36.543135, 128.794986) },
+    { latlng: new kakao.maps.LatLng(36.542661, 128.794429) },
+    { latlng: new kakao.maps.LatLng(36.542399, 128.794058) },
+    { latlng: new kakao.maps.LatLng(36.542253, 128.794494) },
+    { latlng: new kakao.maps.LatLng(36.541748, 128.794732) },
+    { latlng: new kakao.maps.LatLng(36.541122, 128.795045) },
+    { latlng: new kakao.maps.LatLng(36.541867, 128.795365) }
+	];  
